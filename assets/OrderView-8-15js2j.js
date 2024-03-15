591b5d61151b2bbed3a53a58c9d26e1dd78fcc26
colorgolden <147834687+colorgolden@users.noreply.github.com>
2024-03-15 19:03:10 +0800

    Updates

---
+++ assets/OrderView-8-15js2j.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,b as t,F as n,a as c,o as s,r as a}from"./index-LjKqdEST.js";const _={},l=c("h2",null,"OrderView",-1);function d(i,m){const e=a("RouterView");return s(),r(n,null,[l,t(e)],64)}const f=o(_,[["render",d]]);export{f as default};
--- dist/assets/OrderView-EijVZRyN.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,b as t,F as n,a as c,o as s,r as a}from"./index-KoWBsS6M.js";const _={},l=c("h2",null,"OrderView",-1);function d(i,m){const e=a("RouterView");return s(),r(n,null,[l,t(e)],64)}const f=o(_,[["render",d]]);export{f as default};
